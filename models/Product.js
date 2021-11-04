654692218e16535c7b692714cc08bc1e7796f2fa
Joe Kowalczyk <josephdkowalczyk61@gmail.com>
2021-11-04 18:29:33 -0500

    finished defining colums in the modeles

--- models/Product.js
+++ models/Product.js
@@ -11,27 +11,27 @@ Product.init(
   {
     // define columns
     id: {
-      type: dataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
       autoIncrement: true
     },
     product_name: {
-      type: dataTypes.STRING,
+      type: DataTypes.STRING,
       allowNull: false,
       validate: {
         isDecimal: true
       }
     },
     stock: {
-      type: dataTypes.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
       validate:{
         isNumeric: true
       },
       category_id: {
-        type: dataTypes.INTEGER,
+        type: DataTypes.INTEGER,
         references: {
           model: "category",
           key: "id"
